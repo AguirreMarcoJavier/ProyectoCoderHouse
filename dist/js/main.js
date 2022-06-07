f59946a0aaefd8eaafc67b8880e78ec780a57ed2
Marco <103866262+AguirreMarcoJavier@users.noreply.github.com>
2022-06-07 18:06:08 -0300

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,9 @@
-// When the menu button is click then open the nav menu
 $('#menu-button').click(function() {
      $(this).toggleClass('change');
      $('.nav-links').toggleClass('nav-open');
 });
 
-/* If the window is resized and the class change has already been
-applied, then toggle it to avoid an issue with the menu button disappearing */
+
 $(window).resize(function() {
      if ($(window).width() > 768) {
           console.log($(window).width());
